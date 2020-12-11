82848922c5562230ec7c366cabe3454e9cc1108a
mozf041095 <30820220+mozf041095@users.noreply.github.com>
2020-12-11 08:14:44 +0800

    change the blank space

--- BKDRHash/BKDRHash.c
+++ BKDRHash/BKDRHash.c
@@ -1,14 +1,14 @@
 unsigned int BKDRHash(char* str)
 {
-        unsigned int seed = 131; // 31 131 1313 13131 131313 etc..
-            unsigned int hash = 0;
+    unsigned int seed = 131; // 31 131 1313 13131 131313 etc..
+    unsigned int hash = 0;
 
-                while (*str)
-                        {
-                                    hash = hash * seed + (*str++);
-                                        }
+    while (*str)
+    {
+        hash = hash * seed + (*str++);
+    }
 
-                    return (hash & 0x7FFFFFFF);
+    return (hash & 0x7FFFFFFF);
 }
 
 int main(int argc, char* argv[])
